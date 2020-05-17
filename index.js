367d124ea492a4d93d482315f3eb8773bb303196
HyunMo Ahn <gusahdks@gmail.com>
2020-05-17 17:52:38 +0900

    Complete envirment setting

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const runTimeDOM = document.getElementById('run-time');
 const currentTimeDOM = document.getElementById('current-time');
 const deployTimeDOM = document.getElementById('deploy-time');
+const cacheTimeDOM = document.getElementById('cache-time');
 
 function updateCurrentTime() {
   currentTimeDOM.innerHTML = new Date().toISOString();
@@ -17,6 +18,15 @@ function readDeployTime() {
   fetch('../deploy-time.txt')
     .then(response => response.text())
     .then(text => deployTimeDOM.innerHTML = text)
+    .then(() => {
+      const cacheTime = +new Date(cacheTimeDOM.innerHTML)
+      const deployTime = +new Date(deployTimeDOM.innerHTML)
+
+      console.log(cacheTime, deployTime)
+      if (cacheTime !== deployTime) {
+        cacheTimeDOM.classList.add('red')
+      }
+    })
 }
 
 function init() {
